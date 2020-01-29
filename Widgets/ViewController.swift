cdc6768aeb272446f6b4070ad92dcb98f2e1456b
Vladyslav <>
2020-01-29 13:50:58 +0200

    Initial

---
+++ Widgets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Widgets
+//
+//  Created by Vladyslav Tsykhmistro on 29.01.2020.
+//  Copyright © 2020 Vladyslav Tsykhmistro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
